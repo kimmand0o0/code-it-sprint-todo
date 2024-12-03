84659dd82c78a3dedfd6ffb9e42f4b668e7bc0bd
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2024-12-03 12:48:25 +0900

    💄 header의 z-index를 가장 상위로 올림

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -13,7 +13,7 @@ const Header: FC = () => {
   const router = useRouter();
 
   return (
-    <header className="fixed top-0 left-0 w-full h-[60px] bg-white border-slate-200 border-b-[1px] flex justify-center items-center">
+    <header className="fixed z-50 top-0 left-0 w-full h-[60px] bg-white border-slate-200 border-b-[1px] flex justify-center items-center">
       <div className={`body-wrapper ${useWindowWidth() > 600 && "p-5"}`}>
         <button
           onClick={() => {
