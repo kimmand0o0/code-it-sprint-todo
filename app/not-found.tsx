4fb336ee832d36f4dffd33a71318e2b391f437bf
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2024-12-03 14:43:58 +0900

    🎨 코드 포맷팅

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>요청하신 페이지는 존재하지 않습니다.</div>;
+  return (
+    <div className="size-full flex justify-center items-center">
+      요청하신 페이지는 존재하지 않습니다.
+    </div>
+  );
 };
 
 export default NotFound;
