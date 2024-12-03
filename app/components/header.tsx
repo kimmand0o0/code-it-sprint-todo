272f402ba4ae23cb8255b9e34c2d016489a4ea16
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2024-12-03 09:32:56 +0900

    ⚡️ 로고 클릭 시 메인 화면으로 이동하도록 개선

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,6 +2,7 @@
 
 import { FC } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import LogoLarge from "@/app/assets/images/logo-large.svg";
 import LogoSmall from "@/app/assets/images/logo-small.svg";
@@ -9,13 +10,19 @@ import LogoSmall from "@/app/assets/images/logo-small.svg";
 import useWindowWidth from "@/app/utils/getWindowWidth";
 
 const Header: FC = () => {
+  const router = useRouter()
+
   return (
     <header className="fixed top-0 left-0 w-full h-[60px] bg-white border-slate-200 border-b-[1px] flex justify-center items-center">
       <div className={`body-wrapper ${useWindowWidth() > 600 && "p-5"}`}>
-        <Image
-          src={useWindowWidth() < 600 ? LogoSmall : LogoLarge}
-          alt="logo-large"
-        />
+        <button onClick={() => {
+          router.push('/')
+        }}>
+          <Image
+            src={useWindowWidth() < 600 ? LogoSmall : LogoLarge}
+            alt="logo-large"
+          />
+        </button>
       </div>
     </header>
   );
